02e49a054ee8552a0f4cb6d1df6f1ee7d31cf06e
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-11-19 21:38:13 +0000

    add lost icon

---
+++ client/src/components/shared/icons/CaretRight.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CaretRight = props =>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8" { ...props }>
+    <path d="M0 0v8l4-4-4-4z" transform="translate(2)" />
+  </svg>
+
+export default CaretRight
